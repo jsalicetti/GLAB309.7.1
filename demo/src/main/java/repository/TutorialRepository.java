4c808aba0589f63cb6f0d403d3212d60333d31e9
jsalicetti <jessicasalicetti@yahoo.com>
2024-09-17 10:11:48 -0400

    changes made for lab 310.1.1

--- demo/src/main/java/repository/TutorialRepository.java
+++ demo/src/main/java/repository/TutorialRepository.java
@@ -2,11 +2,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
-
-@Repository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(path = "learningtutorials")
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>
-        // Tutorial → Entity type and Long --> primary key
 {
-    //   findByPublished is a custom method()
+    // findByPublished is a custom method()
     List<Tutorial> findByPublished(boolean published);
 }
+
